4f809ba47d048155daf64577b84c31d7dde30eb9
Jot10 <111444746+Jot10@users.noreply.github.com>
2024-07-19 20:44:24 +0530

    Add files via upload

---
+++ const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color textcolor= Colors.white;
+const Color appcolor= Colors.grey;
+const Color whitecolor=Colors.white;
+const Color blackcolor= Colors.black;
---
+++ const/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:movie_streaming/const/colors.dart';
+
+
+const textStyleW700= TextStyle(fontWeight: FontWeight.w700,color:textcolor);
+const textstylewhite= TextStyle(color: textcolor);
+const textstylegrey=TextStyle(color: Colors.grey);
